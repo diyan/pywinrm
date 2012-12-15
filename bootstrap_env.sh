d9f439e11d524b17d60d2c3deca148c095c59066
Alexey Diyan <alexey.diyan@gmail.com>
2012-12-15 18:39:49 +0200

    Update bootstrap_env command. Support Windows and Linux OS

--- bootstrap_env.sh
+++ bootstrap_env.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 python setup.py bootstrap_env
+read -p 'Press [Enter] key to continue...'
--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from distutils.core import setup, Command
 import os, sys, urllib
+from distutils.core import Command, setup
+
+project_name = 'pywinrm'
 
 class BootstrapEnvironmentCommand(Command):
     description = 'create project development environment from scratch'
@@ -24,21 +26,24 @@ class BootstrapEnvironmentCommand(Command):
         """Creates Python virtual environment if it not exists or re-create if outdated."""
         if not os.path.exists(self.temp_dir):
             os.makedirs(self.temp_dir)
-        virtualenv_setup_path = os.path.join(self.temp_dir, 've-setup.py')
-        urllib.urlretrieve('http://tiny.cc/ve-setup', virtualenv_setup_path)
 
         if not os.path.exists(self.virtualenv_dir):
-            os.system('python {0} --prompt=(pywinrm_env) --no-site-packages env'.format(virtualenv_setup_path))
-
-        self.activate_virtual_environment()
-        os.system('pip install --requirement=requirements.txt --download-cache={0}'.format(self.temp_dir))
+            virtualenv_setup_path = os.path.join(self.temp_dir, 'virtualenv.py')
+            urllib.urlretrieve('https://raw.github.com/pypa/virtualenv/master/virtualenv.py', virtualenv_setup_path)
+            virtualenv_prompt = '\({0}\)'.format(project_name) if os.name == 'posix' else '({0})'.format(project_name)
+            os.system('python {0} --prompt={1} env'.format(virtualenv_setup_path, virtualenv_prompt))
+            self.activate_virtual_environment()
+            easy_install_setup_path = os.path.join(self.temp_dir, 'ez_setup.py')
+            urllib.urlretrieve('http://peak.telecommunity.com/dist/ez_setup.py', easy_install_setup_path)
+            os.system('python {0} -U setuptools'.format(easy_install_setup_path))
+            os.system('pip install --requirement=requirements.txt --download-cache={0}'.format(self.temp_dir))
 
     def activate_virtual_environment(self):
         """ Activates virtual environment in the same way as activate.bat or activate.sh from virtualenv package """
         if not hasattr(sys, 'real_prefix') or 'VIRTUAL_ENV' not in os.environ:
             activate_this = '{0}/env/bin/activate_this.py'.format(self.project_dir)
             virtual_env_path = '{0}/env/bin:'.format(self.project_dir)
-            if os.name == 'nt':
+            if os.name != 'posix':
                 activate_this = '{0}/env/Scripts/activate_this.py'.format(self.project_dir)
                 virtual_env_path = '{0}\env\Scripts;'.format(self.project_dir)
 
@@ -46,7 +51,7 @@ class BootstrapEnvironmentCommand(Command):
             path = os.environ['PATH']
             os.environ['PATH'] = virtual_env_path + path
 
-setup(name='pywinrm',
+setup(name=project_name,
     version='0.0.1',
     description='Python library for Windows Remote Management',
     author='Alexey Diyan',
