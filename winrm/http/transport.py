2bcb5215e8545d212e79da4c4ddb5a8b97ebfdf8
Alexey Diyan <alexey.diyan@gmail.com>
2013-01-07 15:09:51 +0200

    Transport. Support kerberos authentication

--- winrm/http/transport.py
+++ winrm/http/transport.py
@@ -1,13 +1,23 @@
 import sys
+import base64
 from winrm.exceptions import WinRMTransportError
+
+HAVE_KERBEROS=False
+try:
+    import kerberos
+    HAVE_KERBEROS=True
+except ImportError:
+    pass
 
 is_py2 = sys.version[0] == '2'
 if is_py2:
     from urllib2 import Request, URLError, HTTPError, HTTPBasicAuthHandler, HTTPPasswordMgrWithDefaultRealm
     from urllib2 import urlopen, build_opener, install_opener
+    from urlparse import urlparse
 else:
     from urllib.request import Request, URLError, HTTPError, HTTPBasicAuthHandler, HTTPPasswordMgrWithDefaultRealm
     from urllib.request import urlopen, build_opener, install_opener
+    from urllib.parse import urlparse
 
 class HttpTransport(object):
     def __init__(self, endpoint, username, password):
@@ -16,6 +26,28 @@ class HttpTransport(object):
         self.password = password
         self.user_agent =  'Python WinRM client'
         self.timeout = 3600 # Set this to an unreasonable amount for now because WinRM has timeouts
+
+    def basic_auth_only(self):
+        #here we should remove handler for any authentication handlers other than basic
+        # but maybe leave original credentials
+
+        # auths = @httpcli.www_auth.instance_variable_get('@authenticator')
+        # auths.delete_if {|i| i.scheme !~ /basic/i}
+        # drop all variables in auths if they not contains "basic" as insensitive.
+        pass
+
+    def no_sspi_auth(self):
+        # here we should remove handler for Negotiate/NTLM negotiation
+        # but maybe leave original credentials
+        pass
+
+class HttpPlaintext(HttpTransport):
+    def __init__(self, endpoint, username='', password='', disable_sspi=True, basic_auth_only=True):
+        super(HttpPlaintext, self).__init__(endpoint, username, password)
+        if disable_sspi:
+            self.no_sspi_auth()
+        if basic_auth_only:
+            self.basic_auth_only()
 
     def send_message(self, message):
         headers = {'Content-Type' : 'application/soap+xml;charset=UTF-8',
@@ -49,28 +81,6 @@ class HttpTransport(object):
         except URLError as ex:
             raise WinRMTransportError(ex.reason)
 
-    def basic_auth_only(self):
-        #here we should remove handler for any authentication handlers other than basic
-        # but maybe leave original credentials
-
-        # auths = @httpcli.www_auth.instance_variable_get('@authenticator')
-        # auths.delete_if {|i| i.scheme !~ /basic/i}
-        # drop all variables in auths if they not contains "basic" as insensitive.
-        pass
-
-    def no_sspi_auth(self):
-        # here we should remove handler for Negotiate/NTLM negotiation
-        # but maybe leave original credentials
-        pass
-
-class HttpPlaintext(HttpTransport):
-    def __init__(self, endpoint, username='', password='', disable_sspi=True, basic_auth_only=True):
-        super(HttpPlaintext, self).__init__(endpoint, username, password)
-        if disable_sspi:
-            self.no_sspi_auth()
-        if basic_auth_only:
-            self.basic_auth_only()
-
 class HttpSSL(HttpTransport):
     """Uses SSL to secure the transport"""
     def __init__(self, endpoint, username, password, ca_trust_path=None, disable_sspi=True, basic_auth_only=True):
@@ -83,35 +93,86 @@ class HttpSSL(HttpTransport):
         if basic_auth_only:
             self.basic_auth_only()
 
-class HttpGSSAPI(HttpTransport):
-    def __init__(self, endpoint, realm, service=None, keytab=None, opts=None):
+class KerberosTicket:
+    """
+    Implementation based on http://ncoghlan_devs-python-notes.readthedocs.org/en/latest/python_kerberos.html
+    """
+    def __init__(self, service):
+        ignored_code, krb_context = kerberos.authGSSClientInit(service)
+        kerberos.authGSSClientStep(krb_context, '')
+        # TODO authGSSClientStep may raise following error:
+        #GSSError: (('Unspecified GSS failure.  Minor code may provide more information', 851968), ("Credentials cache file '/tmp/krb5cc_1000' not found", -1765328189))
+        self._krb_context = krb_context
+        gss_response = kerberos.authGSSClientResponse(krb_context)
+        self.auth_header = 'Negotiate {0}'.format(gss_response)
+
+    def verify_response(self, auth_header):
+        # Handle comma-separated lists of authentication fields
+        for field in auth_header.split(','):
+            kind, ignored_space, details = field.strip().partition(' ')
+            if kind.lower() == 'negotiate':
+                auth_details = details.strip()
+                break
+        else:
+            raise ValueError('Negotiate not found in {0}'.format(auth_header))
+            # Finish the Kerberos handshake
+        krb_context = self._krb_context
+        if krb_context is None:
+            raise RuntimeError('Ticket already used for verification')
+        self._krb_context = None
+        kerberos.authGSSClientStep(krb_context, auth_details)
+        print('User {0} authenticated successfully using Kerberos authentication'.format(kerberos.authGSSClientUserName(krb_context)))
+        kerberos.authGSSClientClean(krb_context)
+
+class HttpKerberos(HttpTransport):
+    def __init__(self, endpoint, realm=None, service='HTTP', keytab=None):
         """
-        Uses Kerberos/GSSAPI to authenticate and encrypt messages
+        Uses Kerberos/GSS-API to authenticate and encrypt messages
         @param string endpoint: the WinRM webservice endpoint
         @param string realm: the Kerberos realm we are authenticating to
         @param string service: the service name, default is HTTP
         @param string keytab: the path to a keytab file if you are using one
         """
-        super(HttpGSSAPI, self).__init__(endpoint)
-        # Remove the GSSAPI auth from HTTPClient because we are doing our own thing
-        #Ruby
-        #auths = @httpcli.www_auth.instance_variable_get('@authenticator')
-        #auths.delete_if {|i| i.is_a?(HTTPClient::SSPINegotiateAuth)}
-        #service ||= 'HTTP'
-        #@service = "#{service}/#{@endpoint.host}@#{realm}"
-        #init_krb
+        if not HAVE_KERBEROS:
+            raise WinRMTransportError('kerberos is not installed')
 
-        def set_auth(self, username, password):
-            raise NotImplementedError
+        super(HttpKerberos, self).__init__(endpoint, None, None)
+        self.krb_service = '{0}@{1}'.format(service, urlparse(endpoint).hostname)
+        #self.krb_ticket = KerberosTicket(krb_service)
 
-        def send_request(self, message):
-            raise NotImplementedError
+    def set_auth(self, username, password):
+        raise NotImplementedError
 
-        def _init_krb(self):
-            raise NotImplementedError
+    def send_message(self, message):
+        # TODO current implementation does negotiation on each HTTP request which is not efficient
+        # TODO support kerberos session with message encryption
+        krb_ticket = KerberosTicket(self.krb_service)
+        headers = {'Authorization': krb_ticket.auth_header,
+                   'Connection': 'Keep-Alive',
+                   'Content-Type': 'application/soap+xml;charset=UTF-8',
+                   'User-Agent': 'Python WinRM client'}
 
-        def _winrm_encrypt(self, string):
-            raise NotImplementedError
+        request = Request(self.endpoint, data=message, headers=headers)
+        try:
+            response = urlopen(request, timeout=self.timeout)
+            krb_ticket.verify_response(response.headers['WWW-Authenticate'])
+            response_text = response.read()
+            return response_text
+        except HTTPError as ex:
+            #if ex.code == 401 and ex.headers['WWW-Authenticate'] == 'Negotiate, Basic realm="WSMAN"':
+            error_message = 'Kerberos-based authentication was failed. Code {0}'.format(ex.code)
+            if ex.msg:
+                error_message += ', {0}'.format(ex.msg)
+            raise WinRMTransportError(error_message)
+        except URLError as ex:
+            raise WinRMTransportError(ex.reason)
 
-        def _winrm_decrypt(self, string):
-            raise NotImplementedError
+    def _winrm_encrypt(self, string):
+        """
+        @returns the encrypted request string
+        @rtype string
+        """
+        raise NotImplementedError
+
+    def _winrm_decrypt(self, string):
+        raise NotImplementedError
