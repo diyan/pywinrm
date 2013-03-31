3fd323aa2bad80a7b4dcb6ed3e50eeeb8d0dd265
Alexey Diyan <alexey.diyan@gmail.com>
2013-03-31 07:24:28 +0300

    Evaluate CFFI for MIT Kerberos binding

--- tests/test_kerberos_api.py
+++ tests/test_kerberos_api.py
@@ -10,7 +10,7 @@ ffi.cdef("""
 // supported by the platform that has at least 32 bits of precision.
 
 typedef uint32_t gss_uint32;
-/// typedef int32_t gss_int32;
+typedef int32_t gss_int32;
 
 typedef gss_uint32 OM_uint32;
 
@@ -29,27 +29,17 @@ typedef struct gss_buffer_desc_struct {
     void *value;
 } gss_buffer_desc, *gss_buffer_t;
 
-/*
-struct gss_name_struct {
-    size_t length;
-    char *value;
-    gss_OID type;
-};
+typedef struct gss_channel_bindings_struct {
+    OM_uint32 initiator_addrtype;
+    gss_buffer_desc initiator_address;
+    OM_uint32 acceptor_addrtype;
+    gss_buffer_desc acceptor_address;
+    gss_buffer_desc application_data;
+} *gss_channel_bindings_t;
 
-typedef struct gss_name_desc * gss_name_t;
-*/
 
-/* x.c */
 struct gss_name_struct;
-typedef struct gss_name_struct * gss_name_t;
-
-struct gss_name_struct {
-    int x;
-    char *y;
-};
-
-///struct gss_name_struct; // CFFI requires full struct declaration
-///typedef struct gss_name_struct *gss_name_t; // CFFI requires full struct declaration
+typedef struct gss_name_struct *gss_name_t;
 
 struct gss_cred_id_struct;
 typedef struct gss_cred_id_struct * gss_cred_id_t;
@@ -60,9 +50,9 @@ struct gss_ctx_id_struct;
 typedef struct gss_ctx_id_struct * gss_ctx_id_t;
 
 // Credential usage options
-///#define GSS_C_BOTH ...
+#define GSS_C_BOTH ...
 #define GSS_C_INITIATE ...
-///#define GSS_C_ACCEPT ...
+#define GSS_C_ACCEPT ...
 
 
 // Status code types for gss_display_status
@@ -72,13 +62,16 @@ typedef struct gss_ctx_id_struct * gss_ctx_id_t;
 // Various Null values.
 
 #define GSS_C_NO_NAME ...
-///#define GSS_C_NO_BUFFER ...
+#define GSS_C_NO_BUFFER ...
 #define GSS_C_NO_OID ...
 #define GSS_C_NO_OID_SET ...
-///#define GSS_C_NO_CONTEXT ...
-///#define GSS_C_NO_CREDENTIAL ...
-///#define GSS_C_NO_CHANNEL_BINDINGS ...
-///#define GSS_C_EMPTY_BUFFER ... // CompileError: command 'gcc' failed with exit status 1
+#define GSS_C_NO_CONTEXT ...
+#define GSS_C_NO_CREDENTIAL ...
+#define GSS_C_NO_CHANNEL_BINDINGS ...
+
+// FIXME: CFFI failed to process following declaration
+// #define GSS_C_EMPTY_BUFFER {0, NULL}
+// #define GSS_C_EMPTY_BUFFER ...
 
 // Some alternate names for a couple of the above values.  These are defined
 // for V1 compatibility.
@@ -92,7 +85,7 @@ typedef struct gss_ctx_id_struct * gss_ctx_id_t;
 // alternatively may simply equate GSS_C_QOP_DEFAULT to a specific explicit
 // QOP value.  However a value of 0 should always be interpreted by a GSSAPI
 // implementation as a request for the default protection level.
-///#define GSS_C_QOP_DEFAULT ...
+#define GSS_C_QOP_DEFAULT ...
 
 // Expiration time of 2^32-1 seconds means infinite lifetime for a
 // credential or security context
@@ -147,6 +140,22 @@ gss_release_cred(
     gss_cred_id_t *);   /* cred_handle */
 
 OM_uint32
+gss_init_sec_context(
+    OM_uint32 *, /* minor_status */
+    gss_cred_id_t, /* claimant_cred_handle */
+    gss_ctx_id_t *, /* context_handle */
+    gss_name_t, /* target_name */
+    gss_OID, /* mech_type (used to be const) */
+    OM_uint32, /* req_flags */
+    OM_uint32, /* time_req */
+    gss_channel_bindings_t, /* input_chan_bindings */
+    gss_buffer_t, /* input_token */
+    gss_OID *, /* actual_mech_type */
+    gss_buffer_t, /* output_token */
+    OM_uint32 *, /* ret_flags */
+    OM_uint32 *); /* time_rec */
+
+OM_uint32
 gss_display_status(
     OM_uint32 *,        /* minor_status */
     OM_uint32,          /* status_value */
@@ -183,8 +192,8 @@ gss_release_buffer(
 C = ffi.verify(
     """
     #include <gssapi/gssapi.h>
-    /// #include <gssapi/gssapi_generic.h>
-    /// #include <gssapi/gssapi_krb5.h>
+    #include <gssapi/gssapi_generic.h>
+    #include <gssapi/gssapi_krb5.h>
     """,
     #include_dirs=['/usr/include/gssapi'],  # This is not required
     libraries=['gssapi_krb5'])
@@ -196,6 +205,16 @@ class GSSInternalError(Exception):
 
 class GSSError(Exception):
     pass
+
+
+def _gss_buffer_to_str(gss_buffer):
+    out_str = ffi.string(ffi.cast('char *', gss_buffer.value))
+    C.gss_release_buffer(ffi.new('OM_uint32 *'), gss_buffer)
+    return out_str
+
+
+def _str_to_gss_buffer(in_str):
+    return ffi.new('gss_buffer_t', [len(in_str), ffi.new('char[]', in_str)])
 
 
 def validate_gss_status(major_value, minor_value):
@@ -211,8 +230,7 @@ def validate_gss_status(major_value, minor_value):
                                         message_ctx_p, status_str_buf)
     if major_status != 0:
         raise GSSInternalError('Failed to get GSS major display status for last API call')
-    major_status_str = ffi.string(ffi.cast('char *', status_str_buf.value))
-    C.gss_release_buffer(minor_status_p, status_str_buf)
+    major_status_str = _gss_buffer_to_str(status_str_buf)
 
     mech_type = ffi.new('gss_OID', [C.GSS_C_NULL_OID])
     major_status = C.gss_display_status(minor_status_p, minor_value,
@@ -220,8 +238,7 @@ def validate_gss_status(major_value, minor_value):
                                         message_ctx_p, status_str_buf)
     if major_status != 0:
         raise GSSInternalError('Failed to get GSS minor display status for last API call')
-    minor_status_str = ffi.string(ffi.cast('char *', status_str_buf.value))
-    C.gss_release_buffer(minor_status_p, status_str_buf)
+    minor_status_str = _gss_buffer_to_str(status_str_buf)
     # TODO investigate how to de-allocate memory
     raise GSSError((major_status_str, major_value), (minor_status_str, minor_value))
 
@@ -234,55 +251,26 @@ def authenticate_gss_client_init(service, principal):
         raise GSSError('Principal was not provided. Please specify principal in "username@realm" format')
 
     minor_status_p = ffi.new('OM_uint32 *')
-    service_buf = ffi.new('gss_buffer_t', [len(service), ffi.new('char[]', service)])
-    nt_service_name_type = ffi.new('gss_OID', C.GSS_C_NT_HOSTBASED_SERVICE[0])
-    # Investigate how to init gss_name_t * with GSS_C_NO_NAME value
-    #output_name_p = ffi.new('gss_name_t *', C.GSS_C_NO_NAME[0])
-    #out_server_name_p = ffi.new('gss_name_t', [0, ffi.new('gss_OID', [C.GSS_C_NO_NAME]), ffi.new('char[]', '')])
-    out_server_name_p = ffi.new('gss_name_t')
 
-    major_status = C.gss_import_name(minor_status_p, service_buf, nt_service_name_type, out_server_name_p)
+    service_buf = _str_to_gss_buffer(service)
+    out_server_name_p = ffi.new('gss_name_t *')
+    major_status = C.gss_import_name(minor_status_p, service_buf, C.GSS_C_NT_HOSTBASED_SERVICE, out_server_name_p)
     validate_gss_status(major_status, minor_status_p[0])
 
-    principal_buf = ffi.new('gss_buffer_t', [len(principal), ffi.new('char[]', principal)])
-    nt_user_name_type = ffi.new('gss_OID', C.GSS_C_NT_USER_NAME[0])
-    out_user_name_p = ffi.new('gss_name_t *')
-    major_status = C.gss_import_name(minor_status_p, principal_buf, nt_user_name_type, out_user_name_p)
+    gss_ctx_id_p = ffi.new('gss_ctx_id_t *')
+    gss_flags = 0
+    input_token = ffi.new('gss_buffer_t')
+    output_token = ffi.new('gss_buffer_t')
+    major_status = C.gss_init_sec_context(
+        minor_status_p, ffi.NULL, gss_ctx_id_p, out_server_name_p[0],
+        ffi.cast('gss_OID', C.GSS_C_NO_OID), gss_flags, 0,
+        ffi.cast('gss_channel_bindings_t', C.GSS_C_NO_CHANNEL_BINDINGS),
+        input_token,
+        ffi.cast('gss_OID *', C.GSS_C_NO_OID), output_token, ffi.NULL,
+        ffi.new('OM_uint32 *', C.GSS_C_INDEFINITE))
     validate_gss_status(major_status, minor_status_p[0])
 
-    #client_credentials = ffi.new('gss_cred_id_t')
-    #major_status = C.gss_acquire_cred(
-    #    minor_status_p, out_user_name_p[0], C.GSS_C_INDEFINITE,
-    #    C.GSS_C_NO_OID_SET, C.GSS_C_INITIATE, client_credentials, ffi.NULL, ffi.NULL)
-    #validate_gss_status(major_status, minor_status_p[0])
 
-
+# FIXME: Investigate how to pass server name and fix following error
+# ('Server not found in Kerberos database', 2529638919))
 authenticate_gss_client_init('HTTP@server-host', 'username@realm')
-
-"""
-138	    if (principal && *principal)
-139	    {
-140	        gss_name_t name;
-141	        principal_token.length = strlen(principal);
-142	        principal_token.value = (char *)principal;
-143
-144	        maj_stat = gss_import_name(&min_stat, &principal_token, GSS_C_NT_USER_NAME, &name);
-145	        if (GSS_ERROR(maj_stat))
-146	        {
-147	            set_gss_error(maj_stat, min_stat);
-148	            ret = AUTH_GSS_ERROR;
-149	            goto end;
-150	        }
-151
-152	        maj_stat = gss_acquire_cred(&min_stat, name, GSS_C_INDEFINITE, GSS_C_NO_OID_SET, GSS_C_INITIATE,
-153	                                    &state->client_creds, NULL, NULL);
-154	        if (GSS_ERROR(maj_stat))
-155	        {
-156	            set_gss_error(maj_stat, min_stat);
-157	            ret = AUTH_GSS_ERROR;
-158	            goto end;
-159	        }
-160
-161	        maj_stat = gss_release_name(&min_stat, &name);
-162	        if (GSS_ERROR(maj_stat))
-"""
