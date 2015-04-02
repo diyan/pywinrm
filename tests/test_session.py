636e97bfeb2d29aa67b667e70ec502612f8c73da
Alexey Diyan <alexey.diyan@gmail.com>
2015-04-02 20:12:33 +0300

    Tests. Add test for Session.run_ps

--- tests/test_session.py
+++ tests/test_session.py
@@ -2,15 +2,26 @@ from winrm import Session
 
 
 def test_run_cmd(protocol_fake):
-    # TODO this test should cover __init__ method
     s = Session('windows-host', auth=('john.smith', 'secret'))
-    s.protocol = protocol_fake
-
     r = s.run_cmd('ipconfig', ['/all'])
 
     assert r.status_code == 0
     assert 'Windows IP Configuration' in r.std_out
     assert len(r.std_err) == 0
+
+
+def test_run_ps(protocol_fake):
+    s = Session('windows-host', auth=('john.smith', 'secret'))
+    r = s.run_ps('hostname')
+
+    assert r.status_code == 0
+    assert 'Windows IP Configuration' in r.std_out
+    assert len(r.std_err) == 0
+
+
+def test_run_ps_with_error(protocol_fake):
+    # TODO test code in Session._clean_error_msg method
+    pass
 
 
 def test_target_as_hostname():
