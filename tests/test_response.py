aed40c1eb98bcb0d0e3359004d81730ca325a569
Alexey Diyan <alexey.diyan@gmail.com>
2015-04-02 20:12:54 +0300

    Tests. Add test for Response.__repr__

---
+++ tests/test_response.py
@@ -0,0 +1,9 @@
+from winrm import Response
+
+
+def test_response_repr():
+    rs = Response((
+        'too long stdout will be truncated',
+        'too long stderr will be truncated',
+        0))
+    assert str(rs) == '<Response code 0, out "too long stdout will", err "too long stderr will">'  # noqa
