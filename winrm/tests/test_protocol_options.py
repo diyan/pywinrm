799ab5c569fceb025f563498d27c065a421784ed
Alexey Diyan <alexey.diyan@gmail.com>
2015-04-02 20:11:43 +0300

    Tests. Delete obsolete module

--- winrm/tests/test_protocol_options.py
+++
@@ -1,8 +0,0 @@
-def test_change_default_timeout(protocol_fake):
-    header = protocol_fake._get_soap_header()
-    assert header['env:Header']['w:OperationTimeout'] == 'PT60S'
-    protocol_fake.timeout = 120
-    header = protocol_fake._get_soap_header()
-    assert header['env:Header']['w:OperationTimeout'] == 'PT120S'
-
-
