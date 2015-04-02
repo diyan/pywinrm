7707b4e4b8a7aaea79e5fde783d61b8a0babd44f
Alexey Diyan <alexey.diyan@gmail.com>
2015-04-02 20:00:24 +0300

    Remove bootstrap scripts

--- bootstrap_env.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cls
-set DIR=%~d0%~p0%
-set PATH=C:\Python27;C:\Program Files\Python27;C:\Program Files (x86)\Python27;C:\Python27\Scripts;C:\Program Files\Python27\Scripts;C:\Program Files (x86)\Python27\Scripts;%PATH%
-cd %DIR%
-python setup.py bootstrap_env
-pause
--- bootstrap_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python setup.py bootstrap_env
-read -p 'Press [Enter] key to continue...'
