4107191045e521b2c6a628377e9eb90788bb3335
Alexey Diyan <alexey.diyan@gmail.com>
2015-04-24 21:27:03 +0300

    Add requests as dependency

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     license='MIT license',
     packages=('winrm', 'winrm.tests'),
     package_data={'winrm.tests': ['*.ps1']},
-    install_requires=['xmltodict'],
+    install_requires=['xmltodict', 'requests'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
