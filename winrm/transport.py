08df770583fc917f9ab24f7facb2d296f895d961
brian.price <brian.price@rackspace.com>
2016-08-29 16:50:31 -0500

    Found a couple of existing issues with unicde/strings and bytes in the later python 3.X issues

--- winrm/transport.py
+++ winrm/transport.py
@@ -1,21 +1,11 @@
 from __future__ import unicode_literals
 from contextlib import contextmanager
 import re
-import sys
 import os
 import weakref
 import distutils
+import six
 
-is_py2 = sys.version[0] == '2'
-
-if is_py2:
-    from urlparse import urlsplit, urlunsplit
-    # use six for this instead?
-    unicode_type = type(u'')
-else:
-    from urllib.parse import urlsplit, urlunsplit
-    # use six for this instead?
-    unicode_type = type(u'')
 
 import requests
 import requests.auth
@@ -163,7 +153,7 @@ class Transport(object):
 
         # urllib3 fails on SSL retries with unicode buffers- must send it a byte string
         # see https://github.com/shazow/urllib3/issues/717
-        if isinstance(message, unicode_type):
+        if isinstance(message, six.string_types):
             message = message.encode('utf-8')
 
         request = requests.Request('POST', self.endpoint, data=message)
@@ -181,7 +171,7 @@ class Transport(object):
             if ex.response.content:
                 response_text = ex.response.content
             else:
-                response_text = ''
+                response_text = ''.encode()
             # Per http://msdn.microsoft.com/en-us/library/cc251676.aspx rule 3,
             # should handle this 500 error and retry receiving command output.
             if b'http://schemas.microsoft.com/wbem/wsman/1/windows/shell/Receive' in message and b'Code="2150858793"' in response_text:
