3e42733bf736898067ba50e83bf35e3e034dcbff
Jordan Borean <jborean93@gmail.com>
2017-02-23 23:47:38 +1000

    Removed xml boiler plate code, created skeletons of initial messages to send to the server and receive

---
+++ winrm/psrp/message_types.py
@@ -0,0 +1,449 @@
+import uuid
+import xmltodict
+
+
+class Colors(object):
+    """
+    [MS-PSRP] v16.0 2016-07-14
+    2.2.3.3 Color
+
+    Valid colors when setting up the Color object
+    """
+    DARK_BLUE = '1'
+    DARK_GREEN = '2'
+    DARK_CYAN = '3'
+    DARK_RED = '4'
+    DARK_MAGENTA = '5'
+    DARK_YELLOW = '6'
+    GRAY = '7'
+    DARK_GREY = '8'
+    BLUE = '9'
+    GREEN = '10'
+    CYAN = '11'
+    RED = '12'
+    MAGENTA = '13'
+    YELLOW = '14'
+    WHITE = '15'
+
+
+class RunspacePoolStates(object):
+    """
+    [MS-PSRP] v16.0 2016-07-14
+    2.2.3.4 RunspacePoolState
+
+    This data type represents the state of a RunspacePool
+    """
+    BEFORE_OPEN = "0"
+    OPENING = "1"
+    OPENED = "2"
+    CLOSED = "3"
+    CLOSING = "4"
+    BROKEN = "5"
+    NEGOTIATION_SENT = "6"
+    NEGOTIATION_SUCCEEDED = "7"
+    CONNECTING = "8"
+    DISCONNECTED = "9"
+
+
+class ObjectTypes(object):
+    @staticmethod
+    def create_coordinate(x, y):
+        """
+        [MS-PSRP] v16.0 2016-07-14
+        2.2.3.1 Coordinates
+
+        This data type represents a position in the screen buffer area of a user interface
+
+        :param x: String - x coordinate
+        :param y: String - y coordinate
+        :return: dict of the Coordinate object
+        """
+        coordinate = {
+            "Obj": {
+                "@N": "Value",
+                "@RefId": ObjectTypes.get_random_ref_id(),
+                "MS": {
+                    "S": {"@N": "T", "#text": "System.Management.Automation.Host.Coordinates"},
+                    "Obj": {
+                        "@N": "V",
+                        "@RefId": ObjectTypes.get_random_ref_id(),
+                        "MS": {
+                            "I32": [
+                                {"@N": "x", "#text": x},
+                                {"@N": "y","#text": y}
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+        return coordinate
+
+    @staticmethod
+    def create_size(width, height):
+        """
+        [MS-PSRP] v16.0 2016-07-14
+        2.2.3.2 Size
+
+        This data type represents a size of a screen buffer area of a user interface
+
+        :param width: String - width of the area
+        :param height: String - height of the area
+        :return: dict of the Size object
+        """
+        size = {
+            "Obj": {
+                "@N": "Value",
+                "@RefId": ObjectTypes.get_random_ref_id(),
+                "MS": {
+                    "S": {"@N": "T", "#text": "System.Management.Automation.Host.Size"},
+                    "Obj": {
+                        "@N": "V",
+                        "@RefId": ObjectTypes.get_random_ref_id(),
+                        "MS": {
+                            "I32": [
+                                {"@N": "width", "#text": width},
+                                {"@N": "height", "#text": height}
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+        return size
+
+    @staticmethod
+    def create_color(color):
+        """
+        [MS-PSRP] v16.0 2016-07-14
+        2.2.3.3 Color
+
+        This data type represents a color used in a user interface
+
+        :param color: The color value, use Colors()
+        :return: dict of the Color object
+        """
+        color = {
+            "Obj": {
+                "@N": "Value",
+                "@RefId": ObjectTypes.get_random_ref_id(),
+                "MS": {
+                    "S": {"@N": "T", "#text": "System.ConsoleColor"},
+                    "I32": {"@N": "V", "#text": color}
+                }
+            }
+        }
+        return color
+
+    @staticmethod
+    def create_ps_thread_option():
+        """
+        [MS-PSRP] v16.0 2016-07-14
+        2.2.3.6 PSThreadOptions
+
+        This data type represents thread options for an application or a
+        higher-layer protocol on the server.
+
+
+        :return: dict of the PSThreadOption object
+        """
+        object_ref_id = ObjectTypes.get_random_ref_id()
+        ps_thread_options = {
+            "Obj": {
+                "@N": "PSThreadOptions",
+                "@RefId": object_ref_id,
+                "TN": {
+                    "@RefId": ObjectTypes.get_random_ref_id(),
+                    "T": [
+                        "System.Management.Automation.Runspaces.PSThreadOptions"
+                        "System.Enum"
+                        "System.ValueType"
+                        "System.Object"
+                    ]
+                },
+                "ToString": "Default",
+                "I32": object_ref_id
+            }
+        }
+        return ps_thread_options
+
+    @staticmethod
+    def create_apartment_state():
+        """
+        [MS-PSRP] v16.0 2016-07-14
+        2.2.3.7 ApartmentState
+
+        This data type represents the apartment state of an application or
+        higher-layer protocol build on top of the PowerShell Remoting
+        Protocol.
+
+        :return: dict of the ApartmentState object
+        """
+        object_ref_id = ObjectTypes.get_random_ref_id()
+        apartment_state = {
+            "Obj": {
+                "@N": "ApartmentState",
+                "@RefId": object_ref_id,
+                "TN": {
+                    "@RefId": ObjectTypes.get_random_ref_id(),
+                    "T": [
+                        "System.Threading.ApartmentState",
+                        "System.Enum",
+                        "System.ValueType",
+                        "System.Object"
+                    ]
+                },
+                "ToString": "MTA",
+                "I32": object_ref_id
+            }
+        }
+        return apartment_state
+
+    @staticmethod
+    def create_host_info():
+        """
+        [MS-PSRP] v16.0 2016-07-14
+        2.2.3.14 HostInfo
+
+        This data type represents host information
+
+        :return: dict of the HostInfo object
+        """
+        host_info = {
+            "Obj": {
+                "@N": "HostInfo",
+                "@RefId": ObjectTypes.get_random_ref_id(),
+                "MS": {
+                    "Obj": {
+                        "@N": "_hostDefaultData",
+                        "@RefId": ObjectTypes.get_random_ref_id(),
+                        "MS": {
+                            "Obj": {
+                                "@N": "data",
+                                "@RefId": ObjectTypes.get_random_ref_id(),
+                                "TN": {
+                                    "@RefId": ObjectTypes.get_random_ref_id(),
+                                    "T": ["System.Collections.Hashtable", "System.Object"]
+                                },
+                                "DCT": {
+                                    "EN": [
+                                        {
+                                            "I32": { "@N": "Key", "#text": "0"},
+                                            "Obj": ObjectTypes.create_color(Colors.GRAY)["Obj"]
+                                        }, {
+                                            "I32": {"@N": "Key", "#text": "1"},
+                                            "Obj": ObjectTypes.create_color(Colors.BLUE)["Obj"]
+                                        }, {
+                                            "I32": {"@N": "Key", "#text": "2"},
+                                            "Obj": ObjectTypes.create_coordinate("0", "4")["Obj"]
+                                        }, {
+                                            "I32": {"@N": "Key", "#text": "3"},
+                                            "Obj": ObjectTypes.create_coordinate("0", "0")["Obj"]
+                                        }, {
+                                            "I32": {"@N": "Key", "#text": "4"},
+                                            "Obj": {
+                                                "@N": "Value",
+                                                "@RefId": ObjectTypes.get_random_ref_id(),
+                                                "MS": {
+                                                    "S": {"@N": "T", "#text": "System.Int32"},
+                                                    "I32": {"@N": "V", "#text": "25"}
+                                                }
+                                            }
+                                        }, {
+                                            "I32": {"@N": "Key", "#text": "5"},
+                                            "Obj": ObjectTypes.create_size("120", "3000")["Obj"]
+                                        }, {
+                                            "I32": {"@N": "Key", "#text": "6"},
+                                            "Obj": ObjectTypes.create_size("120", "79")["Obj"]
+                                        }, {
+                                            "I32": {"@N": "Key", "#text": "7"},
+                                            "Obj": ObjectTypes.create_size("120", "98")["Obj"]
+                                        }, {
+                                            "I32": {"@N": "Key", "#text": "8"},
+                                            "Obj": ObjectTypes.create_size("181", "98")["Obj"]
+                                        }, {
+                                            "I32": {"@N": "Key", "#text": "9"},
+                                            "Obj": {
+                                                "@N": "Value",
+                                                "@RefId": ObjectTypes.get_random_ref_id(),
+                                                "MS": {
+                                                    "S": [
+                                                        {"@N": "T", "#text": "System.String"},
+                                                        {"@N": "V", "#text": "Pywinrm PSRP"}
+                                                    ]
+                                                }
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    },
+                    "B": [
+                        {"@N": "_isHostNull", "#text": "false"},
+                        {"@N": "_isHostUINull", "#text": "false"},
+                        {"@N": "_isHostRawUINull", "#text": "false"},
+                        {"@N": "_useRunspaceHost", "#text": "false"},
+                    ]
+                }
+            }
+        }
+        return host_info
+
+    @staticmethod
+    def get_random_ref_id():
+        return str(uuid.uuid4().int & (1<<32)-1)
+
+
+class SessionCapability(object):
+    def __init__(self):
+        """
+        [MS-PSRP] v16.0 2016-07-14
+        2.2.2.1 SESSION_CAPABILITY Message
+
+        Session capability
+        Direction: Bidirectional
+        Target: RunspacePool
+        """
+        self.message_type = 0x00010002
+        self.initialised = False
+
+    def create_message_data(self, ps_version, protocol_version, serialization_version):
+        self.ps_version = ps_version
+        self.protocol_version = protocol_version
+        self.serialization_version = serialization_version
+
+        message_data = {
+            "Obj": {
+                "@RefId": ObjectTypes.get_random_ref_id(),
+                "MS": {
+                    "Version": [
+                        {"@N": "PSVersion", "#text": ps_version},
+                        {"@N": "protocolversion", "#text": protocol_version},
+                        {"@N": "SerializationVersion", "#text": serialization_version},
+                    ]
+                }
+            }
+        }
+        message_xml = xmltodict.unparse(message_data, full_document=False)
+        # TODO: Add timezone support
+        self.initialised = True
+
+        return message_xml
+
+    def parse_message_data(self, xml):
+        if self.initialised:
+            raise Exception("Cannot parse message data on an already initialised object")
+
+        message_data = xmltodict.parse(xml)
+        version_values = message_data["Obj"]["MS"]["Version"]
+
+        if len(version_values) != 3:
+            raise Exception("Expecting 3 version properties in SESSION_CAPAIBLITY, actual: %d" % len(version_values))
+
+        for version in version_values:
+            attribute_value = version['@N']
+            if attribute_value == 'PSVersion':
+                self.ps_version = version['#text']
+            elif attribute_value == 'protocolversion':
+                self.protocol_version = version['#text']
+            elif attribute_value == 'SerializationVersion':
+                self.serialization_version = version['#text']
+            else:
+                raise Exception("Invalid attribute value '%s'" % attribute_value)
+
+
+class InitRunspacePool(object):
+    def __init__(self):
+        """
+        [MS-PSRP] v16.0 2016-07-14
+        2.2.2.2 INIT_RUNSPACEPOOL Message
+
+        Initialize RunspacePool
+        Direction: Client to Server
+        Target: RunspacePool
+        """
+        self.message_type = 0x00010004
+
+    def create_message_data(self, min_runspaces, max_runspaces):
+        self.min_runspaces = min_runspaces
+        self.max_runspaces = max_runspaces
+        self.ps_thread_options = ObjectTypes.create_ps_thread_option()
+        self.apartment_state = ObjectTypes.create_apartment_state()
+        self.host_info = ObjectTypes.create_host_info()
+        self.application_arguments = None
+
+        message_data = {
+            "Obj": {
+                "@RefId": ObjectTypes.get_random_ref_id(),
+                "MS": {
+                    "I32": [
+                        {"@N": "MinRunspaces", "#text": min_runspaces},
+                        {"@N": "MaxRunspaces", "#text": max_runspaces}
+                    ],
+                    "Obj": [
+                        self.ps_thread_options["Obj"],
+                        self.apartment_state["Obj"],
+                        self.host_info["Obj"]
+                    ],
+                    "Nil": {"@N": "ApplicationArguments"}
+                }
+            }
+        }
+        message_xml = xmltodict.unparse(message_data, full_document=False)
+
+        return message_xml
+
+
+class RunspaceState(object):
+    def __init__(self):
+        """
+        [MS-PSRP] v16.0 2016-07-14
+        2.2.2.9 RUNSPACEPOOL_STATE Message
+
+        State information of a RunspacePool
+        Direction: Server to Client
+        Target: RunspacePool
+        """
+        self.message_type = 0x00021005
+
+    def parse_message_data(self, xml):
+        message_data = xmltodict.parse(xml)
+        raw_state = message_data["Obj"]["MS"]["I32"]
+        attribute = raw_state["@N"]
+        raw_value = raw_state["#text"]
+
+        if attribute != 'RunspaceState':
+            raise Exception("Invalid RUNSPACE_STATE message from the server")
+
+        actual_state = None
+        for key, value in RunspacePoolStates.__dict__.items():
+            if value == raw_value:
+                actual_state = key
+
+        if actual_state is None:
+            raise Exception("Invalid RunspacePoolState value of %s" % raw_value)
+        self.state = raw_value
+        self.friendly_state = actual_state
+
+
+class ApplicationPrivateData(object):
+    def __init__(self):
+        """
+        [MS-PSRP] v16.0 2016-07-14
+        2.2.2.13 APPLICATION_PRIVATE_DATA Message
+
+        Data private to the application using the Powershell Remoting Protocol
+        on the server and client, which is passed by the protocol without
+        interruption
+        Direction: Server to Client
+        Target: RunspacePool
+        """
+        self.message_type = 0x00021009
+
+    def parse_message_data(self, xml):
+        message_data = xmltodict.parse(xml)
+        try:
+            self.bash_version = message_data["Obj"]["MS"]["Obj"]["DCT"]["En"]["Obj"]["DCT"]["En"]["Version"]["#text"]
+        except TypeError:
+            raise Exception("Invalid APPLICATION_PRIVATE_DATA message from the server")
