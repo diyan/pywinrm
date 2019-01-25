c6344c8ac9c53653ed56e72cb66cc97846d6c327
Thomas Grainger <tagrain@gmail.com>
2019-01-25 17:30:05 +0000

    encoding only - remove flake8 changes

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = '0.3.2.dev1'
+__version__ = '0.3.1.dev0'
 project_name = 'pywinrm'
 
 # PyPi supports only reStructuredText, so pandoc should be installed
@@ -23,11 +23,8 @@ setup(
     license='MIT license',
     packages=('winrm', 'winrm.tests'),
     package_data={'winrm.tests': ['*.ps1']},
-    install_requires=[
-        'xmltodict', 'requests>=2.9.1', 'requests_ntlm>=0.3.0', 'six'],
-    extras_require=dict(
-        kerberos=[
-            'requests-kerberos>=0.10.0'], credssp=['requests-credssp>=0.0.1']),
+    install_requires=['xmltodict', 'requests>=2.9.1', 'requests_ntlm>=0.3.0', 'six'],
+    extras_require = dict(kerberos=['requests-kerberos>=0.10.0'], credssp=['requests-credssp>=0.0.1']),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
