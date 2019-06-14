1a973c0dbdcb9b0d8588abcb10381ed12d4470f2
Brian Price <gblump@gmail.com>
2019-06-13 22:50:38 -0500

    Fixing unit tests so we can test proxies.

--- winrm/tests/test_transport.py
+++ winrm/tests/test_transport.py
@@ -1,62 +1,107 @@
 # coding=utf-8
 import os
+import unittest
+
 from winrm.transport import Transport
 
 
-def test_build_session_cert_validate():
-    t_default = Transport(endpoint="Endpoint",
-                          server_cert_validation='validate',
-                          username='test',
-                          password='test',
-                          auth_method='basic',
-                          )
-    t_ca_override = Transport(endpoint="Endpoint",
+class TestTransport(unittest.TestCase):
+    _old_env = None
+
+    def setUp(self) -> None:
+        super(TestTransport, self).setUp()
+        self._old_env = {}
+        os.environ.pop('REQUESTS_CA_BUNDLE', None)
+        os.environ.pop('CURL_CA_BUNDLE', None)
+        os.environ.pop('HTTPS_PROXY', None)
+        os.environ.pop('HTTP_PROXY', None)
+        os.environ.pop('NO_PROXY', None)
+
+    def tearDown(self) -> None:
+        super(TestTransport, self).tearDown()
+        os.environ.pop('REQUESTS_CA_BUNDLE', None)
+        os.environ.pop('CURL_CA_BUNDLE', None)
+        os.environ.pop('HTTPS_PROXY', None)
+        os.environ.pop('HTTP_PROXY', None)
+        os.environ.pop('NO_PROXY', None)
+
+    def test_build_session_cert_validate_1(self):
+        os.environ['REQUESTS_CA_BUNDLE'] = 'path_to_REQUESTS_CA_CERT'
+
+        t_default = Transport(endpoint="Endpoint",
+                              server_cert_validation='validate',
+                              username='test',
+                              password='test',
+                              auth_method='basic',
+                              )
+        t_default.build_session()
+        self.assertEqual('path_to_REQUESTS_CA_CERT', t_default.session.verify)
+
+    def test_build_session_cert_validate_2(self):
+        os.environ['CURL_CA_BUNDLE'] = 'path_to_CURL_CA_CERT'
+
+        t_default = Transport(endpoint="Endpoint",
+                              server_cert_validation='validate',
+                              username='test',
+                              password='test',
+                              auth_method='basic',
+                              )
+        t_default.build_session()
+        self.assertEqual('path_to_CURL_CA_CERT', t_default.session.verify)
+
+    def test_build_session_cert_override_1(self):
+        os.environ['REQUESTS_CA_BUNDLE'] = 'path_to_REQUESTS_CA_CERT'
+
+        t_default = Transport(endpoint="Endpoint",
                               server_cert_validation='validate',
                               username='test',
                               password='test',
                               auth_method='basic',
                               ca_trust_path='overridepath',
                               )
-    try:
-        os.environ['REQUESTS_CA_BUNDLE'] = 'path_to_REQUESTS_CA_CERT'
         t_default.build_session()
-        t_ca_override.build_session()
-        assert(t_default.session.verify == 'path_to_REQUESTS_CA_CERT')
-        assert(t_ca_override.session.verify == 'overridepath')
-    finally:
-        del os.environ['REQUESTS_CA_BUNDLE']
+        self.assertEqual('overridepath', t_default.session.verify)
 
-    try:
+    def test_build_session_cert_override_2(self):
         os.environ['CURL_CA_BUNDLE'] = 'path_to_CURL_CA_CERT'
+
+        t_default = Transport(endpoint="Endpoint",
+                              server_cert_validation='validate',
+                              username='test',
+                              password='test',
+                              auth_method='basic',
+                              ca_trust_path='overridepath',
+                              )
         t_default.build_session()
-        t_ca_override.build_session()
-        assert(t_default.session.verify == 'path_to_CURL_CA_CERT')
-        assert (t_ca_override.session.verify == 'overridepath')
-    finally:
-        del os.environ['CURL_CA_BUNDLE']
+        self.assertEqual('overridepath', t_default.session.verify)
 
+    def test_build_session_cert_ignore_1(self):
+        os.environ['REQUESTS_CA_BUNDLE'] = 'path_to_REQUESTS_CA_CERT'
+        os.environ['CURL_CA_BUNDLE'] = 'path_to_CURL_CA_CERT'
 
-def test_build_session_cert_ignore():
-    t_default = Transport(endpoint="Endpoint",
-                          server_cert_validation='ignore',
-                          username='test',
-                          password='test',
-                          auth_method='basic',
-                          )
-    t_ca_override = Transport(endpoint="Endpoint",
+        t_default = Transport(endpoint="Endpoint",
+                              server_cert_validation='ignore',
+                              username='test',
+                              password='test',
+                              auth_method='basic',
+                              )
+
+        t_default.build_session()
+        self.assertIs(False, t_default.session.verify)
+
+    def test_build_session_cert_ignore_2(self):
+        os.environ['REQUESTS_CA_BUNDLE'] = 'path_to_REQUESTS_CA_CERT'
+        os.environ['CURL_CA_BUNDLE'] = 'path_to_CURL_CA_CERT'
+
+        t_default = Transport(endpoint="Endpoint",
                               server_cert_validation='ignore',
                               username='test',
                               password='test',
                               auth_method='basic',
                               ca_trust_path='boguspath'
                               )
-    try:
-        os.environ['REQUESTS_CA_BUNDLE'] = 'path_to_REQUESTS_CA_CERT'
-        os.environ['CURL_CA_BUNDLE'] = 'path_to_CURL_CA_CERT'
+
         t_default.build_session()
-        t_ca_override.build_session()
-        assert(isinstance(t_default.session.verify, bool) and not t_default.session.verify)
-        assert (isinstance(t_ca_override.session.verify, bool) and not t_ca_override.session.verify)
-    finally:
-        del os.environ['REQUESTS_CA_BUNDLE']
-        del os.environ['CURL_CA_BUNDLE']
+        self.assertIs(False, t_default.session.verify)
+
+
