db550b9235ae9ec2ef5d65af09296fdd8ca4910f
Brian Price <brian.price@rackspace.com>
2019-07-01 12:06:49 -0500

    Adding a unit test.

--- winrm/tests/test_transport.py
+++ winrm/tests/test_transport.py
@@ -100,6 +100,19 @@ class TestTransport(unittest.TestCase):
         t_default.build_session()
         self.assertEqual('overridepath', t_default.session.verify)
 
+    def test_build_session_cert_override_3(self):
+        os.environ['CURL_CA_BUNDLE'] = 'path_to_CURL_CA_CERT'
+
+        t_default = transport.Transport(endpoint="https://example.com",
+                                        server_cert_validation='validate',
+                                        username='test',
+                                        password='test',
+                                        auth_method='basic',
+                                        ca_trust_path=None,
+                                        )
+        t_default.build_session()
+        self.assertEqual(True, t_default.session.verify)
+
     def test_build_session_cert_ignore_1(self):
         os.environ['REQUESTS_CA_BUNDLE'] = 'path_to_REQUESTS_CA_CERT'
         os.environ['CURL_CA_BUNDLE'] = 'path_to_CURL_CA_CERT'
