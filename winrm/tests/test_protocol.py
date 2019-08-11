0021b048d6f61a3528a1f0fc6b2f707c63a96ac1
RWeber <RWeber@onestopsystems.com>
2019-08-10 22:36:55 -0600

    Add a single test for sending input to stdin

--- winrm/tests/test_protocol.py
+++ winrm/tests/test_protocol.py
@@ -41,6 +41,20 @@ def test_get_command_output(protocol_fake):
     protocol_fake.close_shell(shell_id)
 
 
+def test_send_command_input(protocol_fake):
+    shell_id = protocol_fake.open_shell()
+    command_id = protocol_fake.run_command(shell_id, 'cmd')
+    protocol_fake.send_command_input('echo "hello world"')
+    std_out, std_err, status_code = protocol_fake.get_command_output(
+        shell_id, command_id)
+    assert status_code == 0
+    assert b'hello world' in std_out
+    assert len(std_err) == 0
+
+    protocol_fake.cleanup_command(shell_id, command_id)
+    protocol_fake.close_shell(shell_id)
+
+
 def test_set_timeout_as_sec():
     protocol = Protocol('endpoint',
                         username='username',
