810851b269d186c2a12dac951e44dcff5bb949e3
Brian Price <gblump@gmail.com>
2019-11-03 05:45:07 -0600

    Fixing an issue found with the 'vendor' directory

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = '0.4.1beta1'
+__version__ = '0.4.1beta2'
 project_name = 'pywinrm'
 
 # PyPi supports only reStructuredText, so pandoc should be installed
