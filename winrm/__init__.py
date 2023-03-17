218458731d20d532fb49941352bbc804dfe05dd0
Evan Adam <eadam@easyvista.com>
2023-03-17 10:42:00 +0100

    make clean_error_msg public for protocol class users

--- winrm/__init__.py
+++ winrm/__init__.py
@@ -16,6 +16,53 @@ FEATURE_OPERATION_TIMEOUT = True
 FEATURE_PROXY_SUPPORT = True
 
 
+def clean_error_msg(msg):
+    """converts a Powershell CLIXML message to a more human readable string
+    """
+    # TODO prepare unit test, beautify code
+    # if the msg does not start with this, return it as is
+    if msg.startswith(b"#< CLIXML\r\n"):
+        # for proper xml, we need to remove the CLIXML part
+        # (the first line)
+        msg_xml = msg[11:]
+        try:
+            # remove the namespaces from the xml for easier processing
+            msg_xml = _strip_namespace(msg_xml)
+            root = ET.fromstring(msg_xml)
+            # the S node is the error message, find all S nodes
+            nodes = root.findall("./S")
+            new_msg = ""
+            for s in nodes:
+                # append error msg string to result, also
+                # the hex chars represent CRLF so we replace with newline
+                new_msg += s.text.replace("_x000D__x000A_", "\n")
+        except Exception as e:
+            # if any of the above fails, the msg was not true xml
+            # print a warning and return the original string
+            warnings.warn(
+                "There was a problem converting the Powershell error "
+                "message: %s" % (e))
+        else:
+            # if new_msg was populated, that's our error message
+            # otherwise the original error message will be used
+            if len(new_msg):
+                # remove leading and trailing whitespace while we are here
+                return new_msg.strip().encode('utf-8')
+
+    # either failed to decode CLIXML or there was nothing to decode
+    # just return the original message
+    return msg
+
+
+def _strip_namespace(xml):
+    """strips any namespaces from an xml string"""
+    p = re.compile(b"xmlns=*[\"\"][^\"\"]*[\"\"]")
+    allmatches = p.finditer(xml)
+    for match in allmatches:
+        xml = xml.replace(match.group(), b"")
+    return xml
+
+
 class Response(object):
     """Response from a remote command execution"""
     def __init__(self, args):
@@ -54,53 +101,8 @@ class Session(object):
         if len(rs.std_err):
             # if there was an error message, clean it it up and make it human
             # readable
-            rs.std_err = self._clean_error_msg(rs.std_err)
+            rs.std_err = clean_error_msg(rs.std_err)
         return rs
-
-    def _clean_error_msg(self, msg):
-        """converts a Powershell CLIXML message to a more human readable string
-        """
-        # TODO prepare unit test, beautify code
-        # if the msg does not start with this, return it as is
-        if msg.startswith(b"#< CLIXML\r\n"):
-            # for proper xml, we need to remove the CLIXML part
-            # (the first line)
-            msg_xml = msg[11:]
-            try:
-                # remove the namespaces from the xml for easier processing
-                msg_xml = self._strip_namespace(msg_xml)
-                root = ET.fromstring(msg_xml)
-                # the S node is the error message, find all S nodes
-                nodes = root.findall("./S")
-                new_msg = ""
-                for s in nodes:
-                    # append error msg string to result, also
-                    # the hex chars represent CRLF so we replace with newline
-                    new_msg += s.text.replace("_x000D__x000A_", "\n")
-            except Exception as e:
-                # if any of the above fails, the msg was not true xml
-                # print a warning and return the original string
-                warnings.warn(
-                    "There was a problem converting the Powershell error "
-                    "message: %s" % (e))
-            else:
-                # if new_msg was populated, that's our error message
-                # otherwise the original error message will be used
-                if len(new_msg):
-                    # remove leading and trailing whitespace while we are here
-                    return new_msg.strip().encode('utf-8')
-
-        # either failed to decode CLIXML or there was nothing to decode
-        # just return the original message
-        return msg
-
-    def _strip_namespace(self, xml):
-        """strips any namespaces from an xml string"""
-        p = re.compile(b"xmlns=*[\"\"][^\"\"]*[\"\"]")
-        allmatches = p.finditer(xml)
-        for match in allmatches:
-            xml = xml.replace(match.group(), b"")
-        return xml
 
     @staticmethod
     def _build_url(target, transport):
